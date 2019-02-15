92b6e96155a0d19520bdf4acc21f158a1f43e44c
Yuanok <382547456@qq.com>
2019-02-16 01:36:55 +0800

    登录时记住我

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -21,6 +21,13 @@
                         <label for="password">密码：</label>
                         <input type="password" name="password" value="{{ old('password') }}" class="form-control">
                     </div>
+
+                    <div class="form-group">
+                        <div class="form-check">
+                        <input type="checkbox" name="remember" id="exampleCheck1" class="form-check-input">
+                        <label for="exampleCheck1" class="form-check-label">记住我</label>
+                        </div>
+                    </div>
                     
                     <button class="btn btn-primary" type="submit">登录</button>
                 </form>
