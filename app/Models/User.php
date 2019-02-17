981e6fd6550ffe0a0e1fd0c3c9088d4320b2cc34
Yuanok <382547456@qq.com>
2019-02-17 20:53:42 +0800

    首页微博列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,10 @@ class User extends Authenticatable
     {
         return $this->hasMany(Status::class);
     }
+
+    public function feed()
+    {
+        return $this->statuses()
+                    ->orderBy('created_at','desc');
+    }
 }
