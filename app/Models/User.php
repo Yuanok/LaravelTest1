36b75a9c5ef90b86420203e975614163664388b7
Yuanok <382547456@qq.com>
2019-02-18 18:57:39 +0800

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -83,8 +83,8 @@ class User extends Authenticatable
         $this->followings()->detach($user_ids);
     }
 
-    public function isFolloewing($user_ids)
+    public function isFollowing($user_id)
     {
-        return $this->followings()->contains($user_ids);
+        return $this->followings->contains($user_id);
     }
 }
